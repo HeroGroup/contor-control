614ad5dccbf5f73b2fd2fac8780464795e9bd0a6
nhero <navid_hero_1st@yahoo.com>
2020-10-07 10:21:36 +0330

    rollback patterns models

--- app/Pattern.php
+++ app/Pattern.php
@@ -7,9 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Pattern extends Model
 {
     protected $fillable = [
-        'start_time', 'end_time', 'mode_id', 'degree',
-        'max_current', 'minutes_after', 'relay_status', 'off_minutes',
-        'type', // cooling, gateway
+        'cooling_device_id','start_time', 'end_time', 'mode_id', 'degree',
+        // 'max_current', 'minutes_after', 'relay_status', 'off_minutes',
+        // 'type', // cooling, gateway
     ];
 
     public function mode()
